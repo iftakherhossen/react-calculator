17176b783182fb5112efeeefaceebb77040727c2
Iftakher Hossen <iftakher.one@gmail.com>
2022-11-12 11:44:00 +0600

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="mt-5 text-center">
-            <p>Developed by <a href="https://iftakher-hossen.vercel.app/" target="_blank" className="text-decoration-none text-black">Iftakher Hossen</a></p>
+            <p className="text-black text-md">Developed by <a href="https://iftakher-hossen.vercel.app/" target="_blank" className="text-decoration-none text-black">Iftakher Hossen</a></p>
         </footer>
     )
 }
 
-export default Footer
+export default Footer;
