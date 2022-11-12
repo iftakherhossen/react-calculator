dd31dfea9680d1dd2c969da7e94627f26fc07cfd
Iftakher Hossen <iftakher.one@gmail.com>
2022-11-12 11:52:42 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Wrapper from './Components/Wrapper';
-import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="h-[100vh] flex flex-column justify-center items-center bg-sky-200">
+    <div className="h-[100vh] flex justify-center items-center bg-sky-200">
       <Wrapper />
-      <Footer />
     </div>
   );
 }
