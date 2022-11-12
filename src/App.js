350bc39f42984b201ff7817ca9772f766e0f1b48
Iftakher Hossen <iftakher.one@gmail.com>
2022-11-12 11:48:47 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="h-[100vh] flex justify-center items-center bg-sky-100">
+    <div className="h-[100vh] flex flex-column justify-center items-center bg-sky-100">
       <Wrapper />
       <Footer />
     </div>
